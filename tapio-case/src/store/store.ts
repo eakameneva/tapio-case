bf0d50a3b7680deba3bf7b665f2df0ed6a294e19
eakameneva <kate103422@gmail.com>
2025-02-28 18:06:52 +0100

    feat: add redux-persist

--- tapio-case/src/store/store.ts
+++ tapio-case/src/store/store.ts
@@ -1,11 +1,25 @@
 import { configureStore } from "@reduxjs/toolkit";
 import postsReducer from "./slices/postSlice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedPostsReducer = persistReducer(persistConfig, postsReducer);
 
 export const store = configureStore({
   reducer: {
-    posts: postsReducer,
+    posts: persistedPostsReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
